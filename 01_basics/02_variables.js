21a92fed1d263b58adeccf437065aebb4f921234
Monika yadav <140249565+monisanjay9610@users.noreply.github.com>
2023-10-25 11:55:50 +0000

    conversion of datatype

---
+++ 01_basics/02_variables.js
@@ -0,0 +1,39 @@
+const accountId = 143444
+let accountEmail = "monika@gmail.com"
+var accountPassword = "565765"
+accountCity = "jaipur"
+let accountState;
+
+// accountId = 2 // not allowed
+
+accountEmail = "rakesh@gmail.com"
+accountPassword = "6788565"
+accountCity = "dhoolpur"
+
+console.log(accountId)
+
+// prefer not to use var because of issue in block scope and functional scope
+
+console.table([accountId ,accountEmail, accountPassword, accountCity, accountState]);
+
+
+
+
+
+let name = "monika"
+let age = 22
+let isLoggedIn = false
+
+// number => 2 to pawer 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique
+
+
+// object
+
+console.log(typeof 6598)
+console.log(typeof "hitesh")
